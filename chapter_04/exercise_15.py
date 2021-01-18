9e8a7d3af48c65c07b96c6e9896913b0f3e62efb
Pat Clark <patrick.cl@gmail>
2021-01-17 20:05:21 -0500

    add ex15 book solution

--- chapter_04/exercise_15.py
+++ chapter_04/exercise_15.py
@@ -24,3 +24,18 @@ def get_rainfall():
 
 if __name__ == "__main__":
     get_rainfall()
+
+
+# book solution
+# def get_rainfall():
+#     rainfall = {}
+#     while True:
+#         city_name = input('Enter city name: ')
+#         if not city_name:
+#             break
+
+#         mm_rain = input('Enter mm rain: ')
+#         rainfall[city_name] = rainfall.get(city_name, 0) + int(mm_rain)
+
+#     for city, rain in rainfall.items():
+#         print(f'{city}: {rain}')
